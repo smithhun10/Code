febd6b466a75ac4521f500fde51faf99582ed7c0
Michael Bestas <mkbestas@lineageos.org>
2022-10-03 03:49:23 +0300

    fixup! spes: Build fastboot custom HAL
    Change-Id: If8b60462306f2f92789f57749e9c6dcb81e2d096

--- device.mk
+++ device.mk
@@ -240,7 +240,7 @@ PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-custom \
+    android.hardware.fastboot@1.1-impl.custom \
     fastbootd
 
 # GNSS
